144582cabdc4665762f5fbf3e741258b3fd8eaa7
Anastasia Krapivina <krapivina934@gmail.com>
2023-06-14 18:29:50 +0700

    Creation  junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void needToBuyMinLimit() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(999), 1);
+
+    }
+    @Test
+    public void needToBuyMaxLimit() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(1001), 999);
+
+    }
+    @Test
+    public void needToBuyNullLimit() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(1000), 0);
+
+    }
+}
